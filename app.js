187f81ad464f0fe4285183c2ccc07d9d6a1e2f06
Grzegorz Kaczor <grzesiek125@op.pl>
2022-10-06 20:24:58 +0200

    items routes & controllers

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const itemsRoutes = require('./routes/items-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/api/items', itemsRoutes);
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers',
-    'Orgin, X-Requested-With, Content-Type, Accept, Authorization');
+        'Orgin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATH, DELETE');
     next();
 });
 
-app.listen(8000);
+mongoose
+    .connect('mongodb+srv://grzesiek:grzesiek@cluster0.wcshf.mongodb.net/best-bookstore?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(8000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
