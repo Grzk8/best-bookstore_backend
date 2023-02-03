0dd3bd70366d74e9e3986b883ce3c95f5e8ceaf1
Grzegorz Kaczor <grzesiek125@op.pl>
2023-02-03 14:30:04 +0100

    port env

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/users', usersRoutes);
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wcshf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(8000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
